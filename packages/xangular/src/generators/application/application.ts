f14b3d6549b2b04270533572f74ed0b1995ee639
Patrik Simon <patriksimon@outlook.com>
2023-03-22 14:48:17 +0100

    refactor: :recycle: refactor code
    Refactor code.

--- packages/xangular/src/generators/application/application.ts
+++ packages/xangular/src/generators/application/application.ts
@@ -1,12 +1,10 @@
 import { angularInitGenerator, applicationGenerator } from '@nrwl/angular/generators';
 import { Schema as AngularApplicationOptions } from '@nrwl/angular/src/generators/application/schema';
 import { Schema as AngularInitOptions } from '@nrwl/angular/src/generators/init/schema';
-import { addDependenciesToPackageJson, ensurePackage, formatFiles, GeneratorCallback, names, Tree } from '@nrwl/devkit';
+import { addDependenciesToPackageJson, ensurePackage, formatFiles, installPackagesTask, names, Tree } from '@nrwl/devkit';
 import { wrapAngularDevkitSchematic } from '@nrwl/devkit/ngcli-adapter';
 import * as VERSIONS from '../../utils/versions';
 import { ApplicationGeneratorSchema as ApplicationGeneratorOptions, NgAddOptions, NormalizedOptions } from './schema';
-// import { Schema as NgAddOptions } from '@angular/material/schematics/ng-add';
-// src/material/schematics/ng-add
 
 
 
@@ -24,8 +22,6 @@ export default async function (tree: Tree, options: ApplicationGeneratorOptions)
   const type = 'feat';
   const style = 'scss';
 
-  const tasks: GeneratorCallback[] = [];
-
   const angularInitOptions: AngularInitOptions = {
     ...options,
     style,
@@ -42,22 +38,25 @@ export default async function (tree: Tree, options: ApplicationGeneratorOptions)
     minimal: true,
   };
 
-  const qAppGenerator = await applicationGenerator(tree, angularAppOptions);
-  tasks.push(qAppGenerator);
+  await applicationGenerator(tree, angularAppOptions);
 
-  ensurePackage('@angular/material', VERSIONS.ANGULAR_MATERIAL);
-  ensurePackage('@angular/cdk', VERSIONS.ANGULAR_CDK);
-
-  const installTask = addDependenciesToPackageJson(tree, {
+  const addDepsToTargetWorkspace = addDependenciesToPackageJson(tree, {
     '@angular/material': VERSIONS.ANGULAR_MATERIAL,
     '@angular/cdk': VERSIONS.ANGULAR_CDK,
   }, {});
+
+  // await installPackagesTask(tree);
+
+  ensurePackage('@angular/material', VERSIONS.ANGULAR_MATERIAL);
+  ensurePackage('@angular/cdk', VERSIONS.ANGULAR_CDK);
 
   const ngAddMaterialGenerator = wrapAngularDevkitSchematic('@angular/material', 'ng-add');
 
   const ngAddOptions: NgAddOptions = { project: appName, animations: 'enabled', theme: 'indigo-pink', typography: true };
 
   await ngAddMaterialGenerator(tree, ngAddOptions);
+
   await formatFiles(tree);
-  return installTask;
+
+  return addDepsToTargetWorkspace;
 }
